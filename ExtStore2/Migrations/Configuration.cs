dd808e81799eaf194e438e8c7d1945586ef7168d
Websevee <Websevee@gmail.com>
2019-05-24 23:12:10 +0300

    add Migrations

---
+++ ExtStore2/Migrations/Configuration.cs
@@ -0,0 +1,47 @@
+namespace ExtStore2.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using ExtStore2.Models;
+    using System.Web;
+    using Microsoft.AspNet.Identity;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ExtStore2.DAL.ApplicationContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "ExtStore2.DAL.ApplicationContext";
+        }
+
+        protected override void Seed(ExtStore2.DAL.ApplicationContext context)
+        {
+            var userManager = new MyUserManager(new UserStore<User>(context));
+            var roleManager = new MyRoleManager(new RoleStore<MyRole>(context));
+
+            // создаем две роли
+            var role1 = new MyRole { Name = "admin" };
+            var role2 = new MyRole { Name = "user" };
+
+            // добавляем роли в бд
+            roleManager.Create(role1);
+            roleManager.Create(role2);
+
+            // создаем пользователей
+            var user = new User { Email = "admin@mail.ru", UserName = "admin@mail.ru" };
+            string password = "111111";
+            var result = userManager.Create(user, password);
+
+            // если создание пользователя прошло успешно
+            if (result.Succeeded)
+            {
+                // добавляем для пользователя роли
+                userManager.AddToRole(user.Id, role1.Name);
+                userManager.AddToRole(user.Id, role2.Name);
+            }
+        }
+    }
+}
